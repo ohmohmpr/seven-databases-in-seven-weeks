6c47c25055508b7949b84f59b41639194ab22a16
Panyawat Rattana <panyr603@chare.postbox.in.th>
2021-08-24 12:21:05 +0700

    DAY 3

--- postgresql/README.md
+++ postgresql/README.md
@@ -10,3 +10,6 @@ docker exec -it postgresql_db_1 createdb -h localhost -U postgres 7dbs
 docker exec -it postgresql_db_1 psql -h localhost -U postgres -d 7dbs
 
 cat ./script/add_event.sql | docker exec -i postgresql_db_1 psql -U postgres -d 7dbs
+
+
+
--- postgresql/sql.sql
+++ postgresql/sql.sql
@@ -248,3 +248,138 @@ ORDER BY year, month',
 year int,
 jan int, feb int, mar int, apr int, may int, jun int, jul int, aug int, sep int, oct int, nov int, dec int
 ) ORDER BY YEAR;
+
+-- day 3
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+CREATE EXTENSION IF NOT EXISTS dict_xsyn;
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS cube;
+
+docker exec -it postgresql_db_1 psql -h localhost -U postgres -d 7dbs -c "SELECT '1'::cube;"
+
+create_movies.sql
+CREATE TABLE genres (
+    name text UNIQUE,
+    position integer
+);
+CREATE TABLE movies (
+    movie_id SERIAL PRIMARY KEY,
+    title text,
+    genre cube
+);
+CREATE TABLE actors (
+    actor_id SERIAL PRIMARY KEY,
+    name text
+);
+CREATE TABLE movies_actors (
+    movie_id integer REFERENCES movies NOT NULL,
+    actor_id integer REFERENCES actors NOT NULL,
+    UNIQUE (movie_id, actor_id)
+);
+CREATE INDEX movies_actors_movie_id ON movies_actors (movie_id);
+CREATE INDEX movies_actors_actor_id ON movies_actors (actor_id);
+CREATE INDEX movies_genres_cube ON movies USING gist (genre);
+
+SELECT title FROM movies WHERE title ILIKE 'stardust%';
+SELECT title FROM movies WHERE title ILIKE 'stardust_%';
+
+SELECT COUNT(*) FROM movies WHERE title !~* '^the.*';
+
+CREATE INDEX movies_title_pattern ON movies (lower(title) text_pattern_ops);
+SELECT levenshtein('bat', 'fads');
+
+SELECT levenshtein('bat', 'fad') fad,
+    levenshtein('bat', 'fat') fat,
+    levenshtein('bat', 'bat') bat;
+
+SELECT movie_id, title FROM movies
+WHERE levenshtein(lower(title), lower('a hard day nght')) <= 3;
+
+SELECT show_trgm('Avatar');
+
+CREATE INDEX movies_title_trigram ON movies
+USING gist (title gist_trgm_ops);
+
+SELECT title
+FROM movies
+WHERE title % 'Avatre';
+
+SELECT title
+FROM movies
+WHERE title @@ 'night & day';
+
+SELECT title
+FROM movies
+WHERE to_tsvector(title) @@ to_tsquery('english', 'night & day');
+
+SELECT to_tsvector('A Hard Day''s Night'),
+    to_tsquery('english', 'night & day');
+
+SELECT to_tsvector('Six Days Seven Nights'),
+    to_tsquery('english', 'night & day');
+
+SELECT to_tsvector('Long Day''s Journey Into Night'),
+    to_tsquery('english', 'night & day');
+
+SELECT *
+FROM movies
+WHERE title @@ to_tsquery('english', 'a');
+
+cat 'pg_config --sharedir' usr/share/postgresql/13/tsearch_data/english.stop
+
+SELECT to_tsvector('english', 'A Hard Day''s Night');
+SELECT to_tsvector('simple', 'A Hard Day''s Night');
+
+\dF
+\dFd
+
+SELECT ts_lexize('english_stem', 'Day''s');
+
+SELECT to_tsvector('german', 'was machst du gerade?');
+
+EXPLAIN
+SELECT *
+FROM movies
+WHERE title @@ 'night & day';
+
+CREATE INDEX movies_title_searchable ON movies
+USING gin(to_tsvector('english', title));
+
+EXPLAIN
+SELECT *
+FROM movies
+WHERE to_tsvector('english', title) @@ 'night & day';
+
+SELECT * FROM actors
+WHERE name = 'Broos Wils';
+
+SELECT *
+FROM actors
+WHERE name % 'Broos Wils';
+
+SELECT title
+FROM movies NATURAL JOIN movies_actors NATURAL JOIN actors
+WHERE metaphone(name, 6) = metaphone('Broos Wils', 6);
+
+SELECT name, dmetaphone(name), dmetaphone_alt(name),
+    metaphone(name, 8), soundex(name)
+FROM actors;
+
+SELECT * FROM actors
+WHERE metaphone(name, 8) % metaphone('Robin Williams', 8)
+ORDER BY levenshtein(lower('Robin Williams'), lower(name));
+
+SELECT *
+FROM actors
+WHERE dmetaphone(name) % dmetaphone('Ron');
+
+SELECT name,
+cube_ur_coord('(0,7,0,0,0,0,0,0,0,7,0,0,0,0,10,0,0,0)', position) as score
+FROM genres g
+WHERE cube_ur_coord('(0,7,0,0,0,0,0,0,0,7,0,0,0,0,10,0,0,0)', position) > 0;
+
+SELECT m.movie_id, m.title
+FROM movies m, (SELECT genre, title FROM movies WHERE title = 'Mad Max') s WHERE cube_enlarge(s.genre, 5, 18) @> m.genre AND s.title <> m.title
+ORDER BY cube_distance(m.genre, s.genre)
+LIMIT 10;
